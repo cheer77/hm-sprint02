80b0f0506f0e2545c4ffc44ad3d42fe155f23313
Oleksandr Chervonniy <sheph@ukr.net>
2024-02-13 20:02:35 +0100

    add home work №2

--- src/s2-homeworks/hw02/tests/deleteAffair.test.tsx
+++ src/s2-homeworks/hw02/tests/deleteAffair.test.tsx
@@ -1,35 +1,51 @@
-import React from 'react'
-import { AffairType, deleteAffair } from '../HW2'
+import { deleteAffair, AffairType } from "../HW2";
 
-let initialState: AffairType[]
+let initialState: AffairType[];
 
 beforeEach(() => {
-    initialState = [
-        { _id: 1, name: 'React', priority: 'high' },
-        { _id: 2, name: 'anime', priority: 'low' },
-        { _id: 3, name: 'games', priority: 'low' },
-        { _id: 4, name: 'work', priority: 'high' },
-        { _id: 5, name: 'html & css', priority: 'middle' },
-    ]
-})
+  initialState = [
+    { _id: 1, name: "React", priority: "high" },
+    { _id: 2, name: "anime", priority: "low" },
+    { _id: 3, name: "games", priority: "low" },
+    { _id: 4, name: "work", priority: "high" },
+    { _id: 5, name: "html & css", priority: "middle" },
+  ];
+});
 
-test('delete 0', () => {
-    const newState = deleteAffair(initialState, 0)
-    expect(newState.length).toBe(5)
-})
-test('delete 1', () => {
-    const newState = deleteAffair(initialState, 1)
-    expect(newState.length).toBe(4)
-})
-test('delete 3', () => {
-    const newState = deleteAffair(initialState, 3)
-    expect(newState.length).toBe(4)
-})
-test('delete 5', () => {
-    const newState = deleteAffair(initialState, 5)
-    expect(newState.length).toBe(4)
-})
-test('delete 6', () => {
-    const newState = deleteAffair(initialState, 6)
-    expect(newState.length).toBe(5)
-})
+test("delete 0", () => {
+  const newState = deleteAffair(initialState, 0);
+  expect(newState.length).toBe(5); // Не должно измениться
+  expect(newState).toEqual(initialState); // Состояние не должно измениться
+});
+
+test("delete 1", () => {
+  const newState = deleteAffair(initialState, 1);
+  expect(newState.length).toBe(4); // Должно уменьшиться на 1
+  expect(newState).not.toContainEqual({
+    _id: 1,
+    name: "React",
+    priority: "high",
+  }); // Должно быть удалено
+});
+
+test("delete 3", () => {
+  const newState = deleteAffair(initialState, 3);
+  expect(newState.length).toBe(4); // Должно уменьшиться на 1
+  expect(newState).not.toContainEqual({
+    _id: 3,
+    name: "games",
+    priority: "low",
+  }); // Должно быть удалено
+});
+
+test("delete 5", () => {
+  const newState = deleteAffair(initialState, 5);
+  expect(newState.length).toBe(4); // Не должно измениться, т.к. нет объекта с _id === 5
+  expect(newState).toEqual(initialState); // Состояние не должно измениться
+});
+
+test("delete 6", () => {
+  const newState = deleteAffair(initialState, 6);
+  expect(newState.length).toBe(5); // Не должно измениться
+  expect(newState).toEqual(initialState); // Состояние не должно измениться
+});
--- src/s2-homeworks/hw02/tests/filterAffairs.test.tsx
+++ src/s2-homeworks/hw02/tests/filterAffairs.test.tsx
@@ -1,31 +1,36 @@
-import React from 'react'
-import { AffairType, filterAffairs } from '../HW2'
+import { filterAffairs, AffairType, AffairPriorityType } from "../HW2";
 
-let initialState: AffairType[]
+let initialState: AffairType[];
 
 beforeEach(() => {
-    initialState = [
-        { _id: 1, name: 'React', priority: 'high' },
-        { _id: 2, name: 'anime', priority: 'low' },
-        { _id: 3, name: 'games', priority: 'low' },
-        { _id: 4, name: 'work', priority: 'high' },
-        { _id: 5, name: 'html & css', priority: 'middle' },
-    ]
-})
+  initialState = [
+    { _id: 1, name: "React", priority: "high" },
+    { _id: 2, name: "anime", priority: "low" },
+    { _id: 3, name: "games", priority: "low" },
+    { _id: 4, name: "work", priority: "high" },
+    { _id: 5, name: "html & css", priority: "middle" },
+  ];
+});
 
-test('filter by all', () => {
-    const newState = filterAffairs(initialState, 'all')
-    expect(newState.length).toBe(5)
-})
-test('filter by high', () => {
-    const newState = filterAffairs(initialState, 'high')
-    expect(newState.length).toBe(2)
-})
-test('filter by middle', () => {
-    const newState = filterAffairs(initialState, 'middle')
-    expect(newState.length).toBe(1)
-})
-test('filter by low', () => {
-    const newState = filterAffairs(initialState, 'low')
-    expect(newState.length).toBe(2)
-})
+test("filter all", () => {
+  const newState = filterAffairs(initialState, "all");
+  expect(newState.length).toBe(5); // Должны вернуться все дела
+});
+
+test("filter high", () => {
+  const newState = filterAffairs(initialState, "high");
+  expect(newState.length).toBe(2); // Должны вернуться только дела с приоритетом 'high'
+  expect(newState.every((affair) => affair.priority === "high")).toBe(true); // Проверяем, что все дела имеют приоритет 'high'
+});
+
+test("filter middle", () => {
+  const newState = filterAffairs(initialState, "middle");
+  expect(newState.length).toBe(1); // Должно вернуться только одно дело с приоритетом 'middle'
+  expect(newState.every((affair) => affair.priority === "middle")).toBe(true); // Проверяем, что все дела имеют приоритет 'middle'
+});
+
+test("filter low", () => {
+  const newState = filterAffairs(initialState, "low");
+  expect(newState.length).toBe(2); // Должны вернуться только дела с приоритетом 'low'
+  expect(newState.every((affair) => affair.priority === "low")).toBe(true); // Проверяем, что все дела имеют приоритет 'low'
+});
